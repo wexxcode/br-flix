ba888c3ab679d46c4fd1f56c9da972069349b37c
Wesley S <wexxcode@gmail.com>
2020-07-30 16:51:16 -0300

    atualizando arquivos

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png'
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
-import ButtonLink from './components/Button';
-
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return(
-        <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="BRFLIX"></img>
-            </a>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-            <ButtonLink className="ButtonLink" href="/">
-                Novo Vídeos
-            </ButtonLink>
-            
-        </nav>
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
